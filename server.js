961c73062f19132a7b858fc8d5fb5443f9a23ec7
Pedro Marti <pedro@cratebind.com>
2021-05-21 18:18:45 -0400

    fix server config

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ import express from "express";
 
 const serveStatic = require("serve-static");
 const path = require("path");
+const dotenv = require("dotenv");
+
+dotenv.config();
 const app = express();
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
 app.get(/.*/, function(req, res) {
   res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
-const port = process.env.PORT || 80;
+
+const port = process.env.PORT || 8081;
 app.listen(port);
 console.log(`App is listening on port: ${port}`);
