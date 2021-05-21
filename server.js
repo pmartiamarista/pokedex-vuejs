05964c0cdef83e384252ac4d466027a233fe4676
Pedro Marti <pedro@cratebind.com>
2021-05-21 18:13:00 -0400

    fix server

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import express from "express";
 const serveStatic = require("serve-static");
 const path = require("path");
 const app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
+app.use("/", serveStatic(path.join(__dirname, "/dist")));
+app.get(/.*/, function(req, res) {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
+});
 const port = process.env.PORT || 80;
 app.listen(port);
+console.log(`App is listening on port: ${port}`);
